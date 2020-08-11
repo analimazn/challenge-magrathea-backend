a24491d47ef7260844b80b92006b88d243d24ed7
analimazn <analimazn@gmail.com>
2020-08-11 13:23:12 -0300

    feat: Created routes of events

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,9 @@ const {
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/meetups', meetupController.list)
-// const controller = require('./controller/order')
+routes.get('/meetups', meetupController.listMeetups)
+
+routes.get('/events/past/:meetupId', eventController.listPastEventsByMeetupId)
+routes.get('/events/current/:meetupId', eventController.listCurrentEventsByMeetupId)
 
 module.exports = routes
