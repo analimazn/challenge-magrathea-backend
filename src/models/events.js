e8b0f3fea160d6f76c2e20d92bbf8da14d517291
analimazn <analimazn@gmail.com>
2020-08-12 02:05:10 -0300

    update: Added new field on the model event

--- src/models/events.js
+++ src/models/events.js
@@ -4,19 +4,19 @@ async function findPastEventsByMeetupId(meetupId) {
   try {
     const events = await knex
       .raw(`
-        select e.id,
-            e.title,
-            e.datetime_init,
-            e.datetime_end,
-            e.participants,
-            e.meetup_id,
-            a.place,
-            a.neighborhood,
-            a.street,
-            a.number,
-            a.code,
+        select e.id as id,
+            e.title as title,
+            e.datetime_init as datetime_init,
+            e.datetime_end as datetime_end,
+            e.participants as participants,
+            e.meetup_id as meetup_id,
+            a.place as place,
+            a.neighborhood as neighborhood,
+            a.street as street,
+            a.number as number,
+            a.code as code,
             c.name as city,
-            s.uf,
+            s.uf as uf,
             cn.name as country
         from addresses as a
         left join events as e on a.id=e.address_id
