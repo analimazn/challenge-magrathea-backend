8827391d53b91cc63bfe9cd1a5b5bdbae57bd710
analimazn <analimazn@gmail.com>
2020-08-12 01:47:23 -0300

    feat: Added images of logos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+
+const path = require('path')
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
@@ -5,6 +8,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({ origin: '*' }))
+app.use('/static', express.static(path.join(__dirname, '../logos')))
 app.use(express.json())
 app.use(routes)
 
