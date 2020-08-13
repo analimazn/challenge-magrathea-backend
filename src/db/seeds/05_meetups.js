1e5dfc053f38fc0772fd504452c3e87f8a94a84d
analimazn <analimazn@gmail.com>
2020-08-12 21:53:53 -0300

    update: Updated identation

--- src/db/seeds/05_meetups.js
+++ src/db/seeds/05_meetups.js
@@ -2,15 +2,15 @@ const meetups = require('../../../public/meetups.json')
 
 exports.seed = function (knex, promise) {
   return knex('meetups').del()
-  .then(function () {
-    const data = meetups.data.map((meetup, index) => {
-      return {
-        id: index,
-        name: String(meetup.name).trim(),
-        url_logo: meetup.logo,
-        description: String(meetup.description).trim()
-      }
-    })
-    return knex('meetups').insert(data)
+    .then(function () {
+      const data = meetups.data.map((meetup, index) => {
+        return {
+          id: index,
+          name: String(meetup.name).trim(),
+          url_logo: meetup.logo,
+          description: String(meetup.description).trim()
+        }
+      })
+      return knex('meetups').insert(data)
     })
 }
